a056a8c49d9ab99692e0d1e64237ea9e9e5c38c2
ReichertA2 <aydee0112@yahoo.com>
2022-06-14 22:23:47 -0500

    added hook and api for keywords

---
+++ src/api/apiKeyword.js
@@ -0,0 +1,14 @@
+import apiClientTokenAuth from "./clientTokenAuth";
+
+const endpoint = "/keyword_results";
+
+const post = async (data, cancelToken) => {
+  console.log("post data: ", data);
+  const response = await apiClientTokenAuth(cancelToken).post(endpoint, data);
+  console.log("post", response.ok);
+  return response.ok;
+};
+
+export default {
+  post,
+};
---
+++ src/hooks/useKeyword.js
@@ -0,0 +1,39 @@
+import { useEffect, useContext } from "react";
+import { CancelToken } from "apisauce";
+import apiKeyword from "../api/apiKeyword";
+import { AppContext } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
+
+export default function useKeyword(user_info) {
+  const { setAlert } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let response;
+    const source = CancelToken.source();
+    // console.log('useCreateUser useEffect: ',user.token)
+
+    const createuser_info = async () => {
+      console.log("useKeyword createuser_info: ", user_info);
+      // console.log('useCreateUser createUsers: ',source.token)
+
+      response = await apiKeyword.post(user_info, source.token);
+      console.log("useKeyword createuser_info: ", response);
+      if (response) {
+        setAlert({ msg: `User_info: ${user_info.first_name} Created`, cat: "success" });
+        console.log("user_info success", response);
+        navigate("/");
+      } else if (response !== undefined && response === false) {
+        setAlert({ msg: `Please Reauthorize Your Account`, cat: "warning" });
+        navigate("/");
+        ///redirect to the login page
+      }
+    };
+    if (user_info?.first_name) {
+      createuser_info();
+    }
+    return () => {
+      source.cancel();
+    };
+  }, [user_info]);
+}
