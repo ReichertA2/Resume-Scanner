6b8382d182fb66c0a13d7e507eda88a96b77078d
ReichertA2 <aydee0112@yahoo.com>
2022-06-15 22:52:42 -0500

    did some changes

--- src/components/JobDescriptionsAccordion.js
+++ src/components/JobDescriptionsAccordion.js
@@ -11,23 +11,33 @@ export default function JobDescriptionsAccordion({job_url}) {
   
   return (
     <div >
+              {job_url.map((url, idx)=>(
+
       <Accordion sx={{width:'70%', backgroundColor:"white"}}>
+
+          {/* // < div key={idx}> */}
+
+        
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
           
         >
-          <Typography>Name: {job_url[0][0]}</Typography>
+          <Typography>Name: {url[0]}</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            {job_url[0][1]}
+            
+            {url[1]}
           </Typography>
         </AccordionDetails>
+        {/* // </div> */}
+
       </Accordion>
       
-     
+      ))}
+
     </div>
   );
 }
