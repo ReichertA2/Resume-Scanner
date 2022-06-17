ae69fc8827e255b7ced781cfd76f0531bbed1f45
ReichertA2 <aydee0112@yahoo.com>
2022-06-17 15:36:59 -0500

    added users first name for avatar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -56,7 +56,7 @@ export default function AccountMenu() {
             aria-haspopup="true"
             aria-expanded={open ? 'true' : undefined}
           >
-            <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+            <Avatar sx={{ width: 32, height: 32 }}>{user.first_name[0]}</Avatar>
           </IconButton>
         </Tooltip>
         
